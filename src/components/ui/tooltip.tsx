e3e5deb136ba8dab8308f07c9a388c5299ddda66
yarabramasta <yarabram111@gmail.com>
2025-05-06 08:47:30 +0700

    fix: refactor tooltip and job title badge

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -79,13 +79,16 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          'bg-popover text-popover-foreground/80 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) rounded-md px-2 py-1 text-xs font-medium text-balance shadow-sm',
+          'bg-popover text-popover-foreground/80 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) overflow-hidden rounded-md px-2 py-1 text-xs font-medium text-balance',
           className
         )}
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="bg-popover fill-popover z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
+        <TooltipPrimitive.Arrow
+          className="bg-popover fill-popover size-2.5 translate-y-[calc(-50%_-2px)] rotate-45 rounded-[2px]"
+          aria-hidden="true"
+        />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,14 @@ import { createFileRoute } from '@tanstack/react-router'
 import { motion } from 'motion/react'
 
 import IcosphereScene from '~/components/icoshpere-scene'
-import { Badge } from '~/components/ui/badge'
+import { badgeVariants } from '~/components/ui/badge'
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger
 } from '~/components/ui/tooltip'
+import { cn } from '~/lib/utils'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent
@@ -32,7 +33,7 @@ function RouteComponent() {
       >
         <div className="mb-4 space-y-2">
           <motion.h1
-            className="text-xl leading-tight font-semibold will-change-auto"
+            className="text-xl leading-tight font-semibold"
             initial={{ opacity: 0, filter: 'blur(8px)' }}
             animate={{ opacity: 1, filter: 'blur(0)' }}
             transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.15 }}
@@ -45,7 +46,7 @@ function RouteComponent() {
           <motion.p
             // eslint-disable-next-line react/no-array-index-key
             key={`introduction-paragraph-key-${index}`}
-            className="text-foreground/80 transform-gpu text-sm leading-loose will-change-auto"
+            className="text-foreground/80 transform-gpu text-sm leading-loose"
             initial={{ opacity: 0, filter: 'blur(4px)', y: 8 }}
             animate={{ opacity: 1, filter: 'blur(0)', y: 0 }}
             transition={{
@@ -65,39 +66,38 @@ function RouteComponent() {
 function JobTitleBadge() {
   return (
     <TooltipProvider>
-      <Tooltip>
+      <Tooltip useTouch>
         <TooltipTrigger asChild>
-          <Badge
+          <motion.h2
             role="button"
-            className="relative w-fit cursor-pointer overflow-hidden rounded-sm will-change-auto"
-            asChild
+            className={cn(
+              badgeVariants({ variant: 'brand' }),
+              'w-fit cursor-pointer overflow-hidden rounded-sm select-none'
+            )}
+            initial={{ opacity: 0, filter: 'blur(4px)' }}
+            animate={{ opacity: 1, filter: 'blur(0)' }}
+            transition={{
+              duration: 0.6,
+              ease: [0.16, 1, 0.3, 1],
+              delay: 0.3
+            }}
           >
-            <motion.h2
-              initial={{ opacity: 0, filter: 'blur(4px)' }}
-              animate={{ opacity: 1, filter: 'blur(0)' }}
+            <motion.span
+              className="block transform-gpu font-mono"
+              initial={{ y: 14 }}
+              animate={{ y: 0 }}
               transition={{
-                duration: 0.6,
-                ease: [0.16, 1, 0.3, 1],
-                delay: 0.3
+                delay: 0.075 + 0.3,
+                ease: [0.33, 1, 0.68, 1],
+                duration: 0.2
               }}
             >
-              <motion.span
-                className="block transform-gpu font-mono will-change-auto"
-                initial={{ y: 14 }}
-                animate={{ y: 0 }}
-                transition={{
-                  delay: 0.075 + 0.3,
-                  ease: [0.33, 1, 0.68, 1],
-                  duration: 0.2
-                }}
-              >
-                Software Engineer
-              </motion.span>
-            </motion.h2>
-          </Badge>
+              Software Engineer
+            </motion.span>
+          </motion.h2>
         </TooltipTrigger>
-        <TooltipContent>
-          <p>
+        <TooltipContent className="ml-2">
+          <p className="w-full max-w-[28ch] md:max-w-full">
             Yeah you're right, a person who sits in front of a computer all day
             long... 😝
           </p>
